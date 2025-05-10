67504e9e6aff6e21b15aaffa8ee0fd0ba206ebc7
filip <filipkk96@gmail.com>
2025-05-10 13:37:31 +0200

    Subnet2 changed

--- virtualmachines.tf
+++ virtualmachines.tf
@@ -12,7 +12,7 @@ resource "azurerm_subnet" "main" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_subnet" "main" {
+resource "azurerm_subnet" "main2" {
   name                 = "scrapAppSubnet2"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
